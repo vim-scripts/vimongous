e69a1e51d777f8502815cade9bc5747ea85a2736
Robert Mitchell <robert.mitchell.iii@gmail.com>
2013-04-16 00:00:00 +0000

    Version 0.1: Initial upload

---
+++ venode-lib.js
@@ -0,0 +1,60 @@
+var mongodb = require("mongodb").MongoClient,
+	util = require("util"),
+	// maximum number of records returned
+	limit = 50,
+	// used to determine if the query is returning a cursor or a concrete object
+	arrayRegex = /\.toArray\(\)$/,
+	countRegex = /\.count\(\)$/,
+	limitRegex = /\.limit\(\d*\)/;
+
+//# Returns Cursor 
+//	Returns true if the specified query will return a cursor object
+function returnsCursor(query) {
+	"use strict";
+	if (query.match(arrayRegex) || query.match(countRegex)) {
+		return false;
+	}
+	return true;
+}
+
+//# Execute Query
+//	Executes the specified query on the specified server
+//  Callback takes (error, results)
+//  results may be a single object or array
+function executeQuery(dsn, queryText, cb, opts) {
+	"use strict";
+	opts = opts || {};
+	opts.limit = opts.limit || limit;
+
+	// we need to convert cursor objects to arrays before returning from the eval() call
+	// mongo cannot return cursor objects to regular JS	
+	if (returnsCursor(queryText)) {
+		// limit results before converting to array, if necessary
+		// any limit() in the query should take precedence over the
+		// default limit applied by venode
+		if (!queryText.match(limitRegex)) {
+			queryText += ".limit(" + opts.limit + ")";
+		}
+		queryText += ".toArray()";
+	}
+
+	mongodb.connect(dsn, function (err, db) {
+		if (err) {
+			console.log(err);
+		} else {
+			db.eval(queryText, { nolock: true }, function (err, results) {
+				cb(err, results);
+				// make sure to close the connection
+				db.close();
+			});
+		}
+	});
+}
+
+function prettyPrint(results, doColors) {
+	"use strict";
+	return util.inspect(results, doColors);
+}
+
+exports.executeQuery = executeQuery;
+exports.prettyPrint = prettyPrint;
